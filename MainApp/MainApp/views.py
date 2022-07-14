bb79655ef939969a7e713de5a6c79a75c9e0638d
Achwale Prajwal Namdevrao <63206330+prajwalprogrammer@users.noreply.github.com>
2022-07-14 10:08:17 +0530

    Update views.py

--- MainApp/MainApp/views.py
+++ MainApp/MainApp/views.py
@@ -3,11 +3,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from googletrans import Translator
 def index (request) :
-    # translator = Translator()
-    # translations = {}
-    # translations = translator.translate('मख्खन लाल').text
-    # print(translations)
-    # return HttpResponse("Hello Geeks")
     if request.method == 'POST':
         val=request.POST['your_name']
         translator = Translator()
